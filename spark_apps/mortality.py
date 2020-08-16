897a6f46f3c10f09bfa6eb4a6bdcce5c5aace1f9
Chao Pang <chaopang229@gmail.com>
2020-08-16 15:52:40 -0400

    added output_folder to the argment list of the mortality spark app

--- spark_apps/mortality.py
+++ spark_apps/mortality.py
@@ -11,7 +11,7 @@ from pyspark.sql import Window as W
 from utils.common import *
 
 
-def main(spark, input_folder):
+def main(spark, input_folder, output_folder):
     sequence_data = spark.read.parquet(os.path.join(input_folder, p.parquet_data_path))
     death = preprocess_domain_table(spark, input_folder, 'death')
     death = death.groupby('person_id').agg(F.max(F.col('death_date')).alias('death_date'))
@@ -20,7 +20,7 @@ def main(spark, input_folder):
         .where('death_date IS NULL OR death_date >= max_event_date') \
         .withColumn('mortality', F.col('death_date').isNotNull().cast('int')) \
         .select('person_id', 'mortality').write.mode('overwrite') \
-        .parquet(os.path.join(input_folder, p.mortality_data_path))
+        .parquet(os.path.join(output_folder, p.mortality_data_path))
 
 
 if __name__ == '__main__':
@@ -31,7 +31,14 @@ if __name__ == '__main__':
                         action='store',
                         help='The path for your input_folder where the sequence data is',
                         required=True)
+    parser.add_argument('-o',
+                        '--output_folder',
+                        dest='output_folder',
+                        action='store',
+                        help='The path for your output_folder',
+                        required=True)
+
     ARGS = parser.parse_args()
 
     spark = SparkSession.builder.appName('Generate Mortality labels').getOrCreate()
-    main(spark, ARGS.input_folder)
+    main(spark, ARGS.input_folder, ARGS.output_folder)
