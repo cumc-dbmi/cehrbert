181849cff63c1917d11f5d1580eb022e215ff42c
Chao Pang <ChaoPang229@gmail.com>
2020-10-21 22:30:45 -0400

    extract a subset of the bert input for the downstream fine-tuning tasks

--- models/evaluation_models.py
+++ models/evaluation_models.py
@@ -7,7 +7,7 @@ from models.custom_layers import get_custom_objects
 
 
 def create_bi_lstm_model(max_seq_length, vocab_size, embedding_size, concept_embeddings):
-    age_of_visit_input = tf.keras.layers.Input(name='ages', shape=(1,))
+    age_of_visit_input = tf.keras.layers.Input(name='age', shape=(1,))
 
     concept_ids = tf.keras.layers.Input(shape=(max_seq_length,), dtype='int32', name='concept_ids')
 
@@ -48,14 +48,15 @@ def create_bi_lstm_model(max_seq_length, vocab_size, embedding_size, concept_emb
 
 
 def create_vanilla_bert_bi_lstm_model(max_seq_length, vanilla_bert_model_path):
-    age_of_visit_input = tf.keras.layers.Input(name='ages', shape=(1,))
+    age_of_visit_input = tf.keras.layers.Input(name='age', shape=(1,))
 
     vanilla_bert_model = tf.keras.models.load_model(vanilla_bert_model_path,
                                                     custom_objects=dict(**get_custom_objects()))
+    bert_inputs = vanilla_bert_model.inputs[0:3]
     contextualized_embeddings, _ = vanilla_bert_model.get_layer('encoder').output
     _, _, embedding_size = contextualized_embeddings.get_shape().as_list()
 
-    mask_input = vanilla_bert_model.inputs[-1]
+    mask_input = bert_inputs[-1]
     mask_embeddings = tf.tile(tf.expand_dims(mask_input == 0, -1, name='expand_mask'),
                               [1, 1, embedding_size], name='tile_mask')
     contextualized_embeddings = tf.math.multiply(contextualized_embeddings,
@@ -85,7 +86,7 @@ def create_vanilla_bert_bi_lstm_model(max_seq_length, vanilla_bert_model_path):
 
     output = output_layer(next_input)
 
-    lstm_with_vanilla_bert = Model(inputs=vanilla_bert_model.inputs + [age_of_visit_input],
+    lstm_with_vanilla_bert = Model(inputs=bert_inputs + [age_of_visit_input],
                                    outputs=output, name='Vanilla_BERT_PLUS_BI_LSTM')
 
     return lstm_with_vanilla_bert
@@ -94,13 +95,14 @@ def create_vanilla_bert_bi_lstm_model(max_seq_length, vanilla_bert_model_path):
 def create_temporal_bert_bi_lstm_model(max_seq_length, temporal_bert_model_path):
     temporal_bert_model = tf.keras.models.load_model(temporal_bert_model_path,
                                                      custom_objects=dict(**get_custom_objects()))
+    bert_inputs = temporal_bert_model.inputs[0:5]
     _, _, embedding_size = temporal_bert_model.get_layer('temporal_encoder').output[
         0].get_shape().as_list()
     contextualized_embeddings, _, _ = temporal_bert_model.get_layer('temporal_encoder').output
 
-    age_of_visit_input = tf.keras.layers.Input(name='ages', shape=(1,))
+    age_of_visit_input = tf.keras.layers.Input(name='age', shape=(1,))
 
-    mask_input = temporal_bert_model.inputs[-1]
+    mask_input = bert_inputs[-1]
     mask_embeddings = tf.cast(
         tf.tile(tf.expand_dims(mask_input == 0, -1, name='expand_mask'), [1, 1, embedding_size],
                 name='tile_mask'), tf.float32, name='cast_mask_embeddings')
@@ -129,5 +131,5 @@ def create_temporal_bert_bi_lstm_model(max_seq_length, temporal_bert_model_path)
 
     output = output_layer(next_input)
 
-    return Model(inputs=temporal_bert_model.inputs + [age_of_visit_input], outputs=output,
+    return Model(inputs=bert_inputs + [age_of_visit_input], outputs=output,
                  name='TEMPORAL_BERT_PLUS_BI_LSTM')
