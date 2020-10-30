304b19c26b2e5f0cdec65a90e336b4c879680a15
Chao Pang <ChaoPang229@gmail.com>
2020-10-30 01:31:29 -0400

    changed a wrong table reference from global_temp.hf_conditions to hf_conditions

--- spark_apps/prediction_cohorts/hf_readmission.py
+++ spark_apps/prediction_cohorts/hf_readmission.py
@@ -8,36 +8,19 @@ WITH hf_concepts AS (
         descendant_concept_id AS concept_id 
     FROM global_temp.concept_ancestor AS ca
     WHERE ca.ancestor_concept_id = 316139
-),
-hf_conditions AS (
-    SELECT
-        *
-    FROM global_temp.condition_occurrence AS co
-    JOIN hf_concepts AS hc
-        ON co.condition_concept_id = hc.concept_id
 )
 
-SELECT
-    c.person_id,
-    c.earliest_visit_start_date AS index_date,
-    c.earliest_visit_occurrence_id AS visit_occurrence_id
-FROM
-(
-    SELECT DISTINCT
-        v.person_id,
-        v.visit_occurrence_id,
-        first(DATE(c.condition_start_date)) OVER (PARTITION BY v.person_id 
-            ORDER BY DATE(c.condition_start_date)) AS earliest_condition_start_date,
-        first(DATE(v.visit_start_date)) OVER (PARTITION BY v.person_id 
-            ORDER BY DATE(v.visit_start_date)) AS earliest_visit_start_date,
-        first(v.visit_occurrence_id) OVER (PARTITION BY v.person_id
-            ORDER BY DATE(v.visit_start_date)) AS earliest_visit_occurrence_id
-    FROM global_temp.visit_occurrence AS v
-    JOIN global_temp.hf_conditions AS c
-        ON v.visit_occurrence_id = c.visit_occurrence_id
-    WHERE v.visit_concept_id IN (9201, 262) --inpatient, er-inpatient
-) c
-WHERE c.earliest_visit_start_date <= c.earliest_condition_start_date
+SELECT DISTINCT
+    v.person_id,
+    v.visit_occurrence_id,
+    v.visit_start_date AS index_date
+FROM global_temp.visit_occurrence AS v
+JOIN global_temp.condition_occurrence AS co
+    ON v.visit_occurrence_id = co.visit_occurrence_id
+JOIN hf_concepts AS hf
+    ON co.condition_concept_id = hf.concept_id
+WHERE v.visit_concept_id IN (9201, 262) --inpatient, er-inpatient
+    AND v.visit_start_date <= co.condition_start_date
 """
 
 HOSPITALIZATION_QUERY = """
@@ -66,7 +49,7 @@ def main(spark_args):
     hospitalization_query = QuerySpec(table_name=HOSPITALIZATION_COHORT,
                                       query_template=HOSPITALIZATION_QUERY,
                                       parameters={})
-    hospitalization = QueryBuilder(cohort_name=HOSPITALIZATION_QUERY,
+    hospitalization = QueryBuilder(cohort_name=HOSPITALIZATION_COHORT,
                                    dependency_list=DEPENDENCY_LIST,
                                    query=hospitalization_query)
 
