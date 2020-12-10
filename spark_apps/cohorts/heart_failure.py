ad0046738fe31f0fcd24dce3eb9c46f32d5fd74c
Xinzhuo Jiang <xj2193@arivu.dbmi.columbia.edu>
2020-12-10 13:41:05 -0500

    modify cohort definition of t2dm and hf

--- spark_apps/cohorts/heart_failure.py
+++ spark_apps/cohorts/heart_failure.py
@@ -1,14 +1,15 @@
 from spark_apps.cohorts.query_builder import QueryBuilder, AncestorTableSpec, QuerySpec
 
 HEART_FAILURE_CONCEPT = 316139
-DIURETIC_CONCEPT_ID = [4186999]
-PACEMAKER_CONCEPT_IDS = [4244395, 4051938, 2107005, 2313863, 4142917, 2106987, 2107003, 4232614,
-                         2106988, 2313861, 2313855, 4204395, 42738824, 42742524, 2001980, 35622031,
-                         42738822, 2107027, 4215909, 2211371, 4049403, 42738819, 42738820, 2313948,
-                         2313860]
+#DIURETIC_CONCEPT_ID = [4186999]
+#PACEMAKER_CONCEPT_IDS = [4244395, 4051938, 2107005, 2313863, 4142917, 2106987, 2107003, 4232614,
+#                         2106988, 2313861, 2313855, 4204395, 42738824, 42742524, 2001980, 35622031,
+#                         42738822, 2107027, 4215909, 2211371, 4049403, 42738819, 42738820, 2313948,
+#                         2313860]
 
 HEART_FAILURE_COHORT_QUERY = """
-WITH hf_concepts AS (
+WITH 
+hf_concepts AS (
     SELECT DISTINCT 
         descendant_concept_id AS concept_id 
     FROM global_temp.concept_ancestor AS ca
@@ -45,23 +46,6 @@ FROM
 WHERE c.earliest_visit_start_date <= c.earliest_condition_start_date
 """
 
-EXCLUDE_PRIOR_DIURETICS_TEMPLATE = """
-WITH diuretics_user AS (
-    SELECT DISTINCT
-        de.person_id,
-        FIRST(DATE(drug_exposure_start_date)) AS drug_exposure_start_date
-    FROM global_temp.drug_exposure AS de 
-    JOIN global_temp.{diuretics_concepts} AS dc
-        ON de.drug_concept_id = dc.concept_id
-    GROUP BY de.person_id
-)
-SELECT DISTINCT
-    c.*
-FROM global_temp.{cohort_table_name} AS c 
-LEFT JOIN diuretics_user AS du
-    ON c.person_id = du.person_id AND c.index_date > du.drug_exposure_start_date
-WHERE du.person_id IS NULL
-"""
 
 EXCLUDE_PRIOR_PACEMAKERS_TEMPLATE = """
 WITH pacemaker_user AS (
@@ -102,30 +86,18 @@ DEFAULT_COHORT_NAME = 'heart_failure'
 
 
 def query_builder():
-    dependency_queries = [QuerySpec(table_name=DIURETICS_INGREDIENT_CONCEPTS,
-                                    query_template=ROLL_UP_DIURETICS_TO_INGREDIENT_TEMPLATE,
-                                    parameters={})]
+    dependency_queries = []
     query = QuerySpec(table_name=DEFAULT_COHORT_NAME,
                       query_template=HEART_FAILURE_COHORT_QUERY,
                       parameters={})
-    post_queries = [QuerySpec(table_name=DEFAULT_COHORT_NAME,
-                              query_template=EXCLUDE_PRIOR_DIURETICS_TEMPLATE,
-                              parameters={'cohort_table_name': DEFAULT_COHORT_NAME,
-                                          'diuretics_concepts': DIURETICS_INGREDIENT_CONCEPTS}),
-                    QuerySpec(table_name=DEFAULT_COHORT_NAME,
-                              query_template=EXCLUDE_PRIOR_PACEMAKERS_TEMPLATE,
-                              parameters={'cohort_table_name': DEFAULT_COHORT_NAME,
-                                          'procedure_concept_ids': ','.join(
-                                              [str(c) for c in PACEMAKER_CONCEPT_IDS])})
-                    ]
 
-    ancestor_table_specs = [AncestorTableSpec(table_name=DIURETICS_ANCESTOR_TABLE,
-                                              ancestor_concept_ids=DIURETIC_CONCEPT_ID,
-                                              is_standard=False)]
+    #ancestor_table_specs = [AncestorTableSpec(table_name=DIURETICS_ANCESTOR_TABLE,
+    #                                          ancestor_concept_ids=DIURETIC_CONCEPT_ID,
+    #                                          is_standard=False)]
 
     return QueryBuilder(cohort_name=DEFAULT_COHORT_NAME,
                         query=query,
                         dependency_list=DEPENDENCY_LIST,
-                        dependency_queries=dependency_queries,
-                        post_queries=post_queries,
-                        ancestor_table_specs=ancestor_table_specs)
+                        dependency_queries=[],
+                        post_queries=[],
+                        ancestor_table_specs=[])
