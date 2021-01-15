cd621f312fa53288645f5d18c20c898ad1fdafcf
Chao Pang <ChaoPang229@gmail.com>
2021-01-15 17:16:16 -0500

    updated the covid definition to change the index_date to be admit_date + 24 hours

--- spark_apps/cohorts/covid.py
+++ spark_apps/cohorts/covid.py
@@ -3,15 +3,17 @@ from spark_apps.cohorts.query_builder import QueryBuilder, QuerySpec
 COVID_COHORT_QUERY = """
 SELECT DISTINCT
     c.person_id,
-    FIRST(index_date) OVER (PARTITION BY person_id ORDER BY index_date, visit_occurrence_id) AS index_date,
+    DATE_ADD(FIRST(index_date) OVER (PARTITION BY person_id ORDER BY index_date, visit_occurrence_id), 1) AS index_date,
     FIRST(visit_occurrence_id) OVER (PARTITION BY person_id ORDER BY index_date, visit_occurrence_id) AS visit_occurrence_id
 FROM
 (
     SELECT DISTINCT
         m.person_id,
-        FIRST(measurement_date) OVER (PARTITION BY person_id ORDER BY measurement_date, visit_occurrence_id) AS index_date,
-        FIRST(visit_occurrence_id) OVER (PARTITION BY person_id ORDER BY measurement_date, visit_occurrence_id) AS visit_occurrence_id
+        FIRST(visit_start_date) OVER (PARTITION BY v.person_id ORDER BY visit_start_date, v.visit_occurrence_id) AS index_date,
+        FIRST(v.visit_occurrence_id) OVER (PARTITION BY v.person_id ORDER BY visit_start_date, v.visit_occurrence_id) AS visit_occurrence_id
     FROM global_temp.measurement AS m
+    JOIN global_temp.visit_occurrence AS v
+        ON m.visit_occurrence_id = v.visit_occurrence_id
     JOIN global_temp.concept AS c
         ON m.value_as_concept_id = c.concept_id
     WHERE m.measurement_concept_id IN (723475,723479,706178,723473,723474,586515,706177,706163,706180,706181)
@@ -21,9 +23,11 @@ FROM
 
     SELECT 
         co.person_id,
-        FIRST(condition_start_date) OVER (PARTITION BY person_id ORDER BY condition_start_date, visit_occurrence_id) AS index_date,
-        FIRST(visit_occurrence_id) OVER (PARTITION BY person_id ORDER BY condition_start_date, visit_occurrence_id) AS visit_occurrence_id
+        FIRST(visit_start_date) OVER (PARTITION BY v.person_id ORDER BY visit_start_date, v.visit_occurrence_id) AS index_date,
+        FIRST(v.visit_occurrence_id) OVER (PARTITION BY v.person_id ORDER BY visit_start_date, v.visit_occurrence_id) AS visit_occurrence_id
     FROM global_temp.condition_occurrence AS co
+    JOIN global_temp.visit_occurrence AS v
+        ON co.visit_occurrence_id = v.visit_occurrence_id
     WHERE co.condition_concept_id IN (4100065, 37311061)
 ) c
 """
@@ -36,6 +40,7 @@ def query_builder():
     query = QuerySpec(table_name=DEFAULT_COHORT_NAME,
                       query_template=COVID_COHORT_QUERY,
                       parameters={})
+
     return QueryBuilder(cohort_name=DEFAULT_COHORT_NAME,
                         dependency_list=DEPENDENCY_LIST,
                         query=query)
