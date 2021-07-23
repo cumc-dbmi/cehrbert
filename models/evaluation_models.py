aa217ab6fc663d5d5bb70e8be9f28905dd3052d7
Chao Pang <ChaoPang229@gmail.com>
2021-07-23 15:52:26 -0400

    added time_stamps and ages to LstmBert validation models

--- models/evaluation_models.py
+++ models/evaluation_models.py
@@ -52,12 +52,14 @@ def create_vanilla_bert_bi_lstm_model(max_seq_length, vanilla_bert_model_path):
 
     vanilla_bert_model = tf.keras.models.load_model(vanilla_bert_model_path,
                                                     custom_objects=dict(**get_custom_objects()))
-    bert_inputs = vanilla_bert_model.inputs[0:4] + [vanilla_bert_model.inputs[-1]]
-#     bert_inputs = vanilla_bert_model.inputs
+    bert_inputs = [i for i in vanilla_bert_model.inputs if 'visit' not in i.name]
+    #     bert_inputs = vanilla_bert_model.inputs
     contextualized_embeddings, _ = vanilla_bert_model.get_layer('encoder').output
     _, _, embedding_size = contextualized_embeddings.get_shape().as_list()
 
-    mask_input = bert_inputs[-1]
+    # mask_input = bert_inputs[-1]
+    mask_input = [i for i in bert_inputs if
+                  'mask' in i.name and 'concept' not in i.name][0]
     mask_embeddings = tf.tile(tf.expand_dims(mask_input == 0, -1, name='expand_mask'),
                               [1, 1, embedding_size], name='tile_mask')
     contextualized_embeddings = tf.math.multiply(contextualized_embeddings,
@@ -77,7 +79,8 @@ def create_vanilla_bert_bi_lstm_model(max_seq_length, vanilla_bert_model_path):
 
     output_layer = tf.keras.layers.Dense(1, activation='sigmoid')
 
-    next_input = masking_layer(contextualized_embeddings) #attach a property to the concept embeddings to indicate where are the masks, send the flag to the downstream layer
+    next_input = masking_layer(
+        contextualized_embeddings)  # attach a property to the concept embeddings to indicate where are the masks, send the flag to the downstream layer
 
     next_input = dropout_lstm_layer(bi_lstm_layer(next_input))
 
