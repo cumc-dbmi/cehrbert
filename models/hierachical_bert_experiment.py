f00d221b37fc2b2c22849d54003e9948f23d496e
Chao Pang <ChaoPang229@gmail.com>
2021-11-16 17:14:31 -0500

    an example for the first level BERT

--- models/hierachical_bert_experiment.py
+++ models/hierachical_bert_experiment.py
@@ -17,6 +17,9 @@ num_seq = num_concept_per_v*num_visit
 vocab_size = 40000
 embeddinig_size = 128
 time_embeddings_size = 16
+depth = 16
+num_heads = 8
+transformer_dropout: float = 0.1
 
 
 # %%
@@ -72,12 +75,136 @@ tf.shape(input_for_encoder)
 
 
 # %%
+def create_concept_mask(mask, max_seq_length):
+    # mask the third dimension
+    concept_mask_1 = tf.tile(tf.expand_dims(tf.expand_dims(mask, axis=1), axis=-1),
+                             [1, 1, 1, max_seq_length])
+    # mask the fourth dimension
+    concept_mask_2 = tf.expand_dims(tf.expand_dims(mask, axis=1), axis=1)
+    concept_mask = tf.cast((concept_mask_1 + concept_mask_2) > 0, dtype=tf.int32)
+    return concept_mask
+
+
+# %%
 concept_mask = create_concept_mask(mask, num_seq)
 
 
 # %%
-concept_mask
+class MultiHeadAttention(tf.keras.layers.Layer):
+
+    def __init__(self, d_model, num_heads, **kwargs):
+        super(MultiHeadAttention, self).__init__(**kwargs)
+
+        self.num_heads = num_heads
+        self.d_model = d_model
+
+        assert d_model % self.num_heads == 0
+
+        self.depth = d_model // self.num_heads
+
+        self.wq = tf.keras.layers.Dense(d_model)
+        self.wk = tf.keras.layers.Dense(d_model)
+        self.wv = tf.keras.layers.Dense(d_model)
+        self.dense = tf.keras.layers.Dense(d_model)
+
+    def get_config(self):
+        config = super().get_config()
+        config['d_model'] = self.d_model
+        config['num_heads'] = self.num_heads
+        return config
+
+    def split_heads(self, x, batch_size):
+        """Split the last dimension into (num_heads, depth).
+        Transpose the result such that the shape is (batch_size, num_heads, seq_len, depth)
+        """
+        x = tf.reshape(x, (batch_size, -1, self.num_heads, self.depth))
+        return tf.transpose(x, perm=[0, 2, 1, 3])
+
+    def split_heads_query_key_value(self, batch_size, k, q, v):
+        q = self.split_heads(q, batch_size)  # (batch_size, num_heads, seq_len_q, depth)
+        k = self.split_heads(k, batch_size)  # (batch_size, num_heads, seq_len_k, depth)
+        v = self.split_heads(v, batch_size)  # (batch_size, num_heads, seq_len_v, depth)
+        return k, q, v
+
+    def call(self, v, k, q, mask, time_attention_logits):
+        batch_size = tf.shape(q)[0]
+
+        q = self.wq(q)  # (batch_size, seq_len, d_model)
+        k = self.wk(k)  # (batch_size, seq_len, d_model)
+        v = self.wv(v)  # (batch_size, seq_len, d_model)
+
+        k, q, v = self.split_heads_query_key_value(batch_size, k, q, v)
+
+        # scaled_attention.shape == (batch_size, num_heads, seq_len_q, depth)
+        # attention_weights.shape == (batch_size, num_heads, seq_len_q, seq_len_k)
+        scaled_attention, attention_weights = scaled_dot_product_attention(q, k, v, mask,
+                                                                           time_attention_logits)
+
+        scaled_attention = tf.transpose(scaled_attention,
+                                        perm=[0, 2, 1,
+                                              3])  # (batch_size, seq_len_q, num_heads, depth)
+
+        concat_attention = tf.reshape(scaled_attention,
+                                      (batch_size, -1,
+                                       self.d_model))  # (batch_size, seq_len_q, d_model)
+
+        output = self.dense(concat_attention)  # (batch_size, seq_len_q, d_model)
+
+        return output, attention_weights
 
 
 # %%
+mha = MultiHeadAttention(embeddinig_size, num_heads)
 
+# %%
+tf.shape(input_for_encoder)
+
+# %%
+# mask = tf.ones((1, 1000))
+# concept_mask = create_concept_mask(mask, num_seq)
+x = tf.random.uniform((1, 50, 20, 128))
+
+# %%
+tf.shape(x)
+
+# %%
+x_reshape = tf.reshape(tf.transpose(x, perm=[0, 2, 1, 3]), (-1, num_concepts, embeddinig_size))
+
+# %%
+mha(x_reshape, x_reshape, x_reshape, None, None)
+
+# %%
+batch_size, num_concepts, num_visits, _ = tf.shape(x)
+
+
+# %%
+x = tf.reshape(x, (batch_size, num_concepts, num_visits, 8, 16))
+
+
+# %%
+tf.transpose(x, perm=[0, 2, 3, 1, 4])
+
+# %%
+encoder = Encoder(name='encoder',
+                  num_layers=depth,
+                  d_model=embeddinig_size,
+                  num_heads=num_heads,
+                  dropout_rate=transformer_dropout)
+
+
+# %%
+contextualized_visit_emebddings, attention_weights = encoder(x_reshape, None)
+
+# %%
+tf.shape(contextualized_visit_emebddings)
+
+# %%
+contextualized_visit_emebddings = tf.reshape(contextualized_visit_emebddings, (-1, 20, 50, embeddinig_size))
+
+# %%
+visit_embeddings = contextualized_visit_emebddings[:, 0]
+
+# %%
+visit_embeddings
+
+# %%
