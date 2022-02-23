d25cc0f733d2ea82b808c770c0414b73a4d0d48c
Chao Pang <ChaoPang229@gmail.com>
2022-02-22 22:36:23 -0500

    changed the sliding window to 5

--- data_generators/data_generator_base.py
+++ data_generators/data_generator_base.py
@@ -293,7 +293,7 @@ class HierarchicalBertDataGenerator(AbstractDataGeneratorBase):
                  concept_tokenizer: ConceptTokenizer,
                  max_num_of_visits: int,
                  max_num_of_concepts: int,
-                 sliding_window: int = 10,
+                 sliding_window: int = 5,
                  min_num_of_concepts: int = 5,
                  *args,
                  **kwargs):
