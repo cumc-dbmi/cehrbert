004c2ab4d9ab736c0e244ba490dc8dc507dff0a3
Chao Pang <ChaoPang229@gmail.com>
2022-03-28 23:26:52 -0400

    removed save_freq from ModelCheckpoint

--- trainers/model_trainer.py
+++ trainers/model_trainer.py
@@ -143,7 +143,6 @@ class AbstractConceptEmbeddingTrainer(AbstractModel):
         model_checkpoint = tf.keras.callbacks.ModelCheckpoint(filepath=self._model_path,
                                                               save_best_only=True,
                                                               monitor='loss',
-                                                              save_freq=20000,
                                                               verbose=1)
         learning_rate_scheduler = tf.keras.callbacks.LearningRateScheduler(
             CosineLRSchedule(lr_high=self._learning_rate, lr_low=1e-8, initial_period=10),
