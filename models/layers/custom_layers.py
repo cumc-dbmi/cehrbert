7b6fd962c45f493b590b6a601935d5bdbf23fab5
Chao Pang <ChaoPang229@gmail.com>
2022-06-18 00:06:19 -0400

    bring phenotype_concept_distance_weight down to 5e-05

--- models/layers/custom_layers.py
+++ models/layers/custom_layers.py
@@ -839,8 +839,8 @@ class VisitPhenotypeLayer(tf.keras.layers.Layer):
             transformer_dropout: float,
             num_of_neighbors: int = 3,
             phenotype_entropy_weight: float = 2e-05,
-            phenotype_euclidean_weight: float = 3e-05,
-            phenotype_concept_distance_weight: float = 6e-05,
+            phenotype_euclidean_weight: float = 2e-05,
+            phenotype_concept_distance_weight: float = 5e-05,
             *args, **kwargs
     ):
         super(VisitPhenotypeLayer, self).__init__(*args, **kwargs)
