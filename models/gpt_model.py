d111c101b102c40da492d08ee9a88b23f5399f12
Chao Pang <ChaoPang229@gmail.com>
2023-02-07 21:45:33 -0500

    updated gpt_model and added the causal mask

--- models/gpt_model.py
+++ models/gpt_model.py
@@ -1,27 +1,49 @@
 import tensorflow as tf
 
 from models.layers.custom_layers import (
-    Encoder, GptDecoder,
+    GptDecoder,
     TokenAndPositionEmbedding
 )
-from utils.model_utils import create_concept_mask
-
-vocab_size = 20000  # Only consider the top 20k words
-maxlen = 80  # Max sequence size
-embed_dim = 256  # Embedding size for each token
-num_heads = 8  # Number of attention heads
-feed_forward_dim = 256  # Hidden layer size in feed forward network inside transformer
 
 
-def create_model():
-    concept_inputs = tf.keras.layers.Input(shape=(maxlen,), dtype=tf.int32)
-    decoder_mask_inputs = tf.keras.layers.Input(shape=(maxlen,), dtype=tf.int32)
-    decoder_mask_inputs_expanded = decoder_mask_inputs[:, tf.newaxis, tf.newaxis, :]
-    embedding_layer = TokenAndPositionEmbedding(maxlen, vocab_size, embed_dim)
+def create_model(
+        max_len,
+        vocab_size,
+        embed_dim,
+        num_heads,
+        num_of_layers
+):
+    """
+    model = create_model(
+        max_len=100,
+        vocab_size=100,
+        embed_dim=128,
+        num_heads=16,
+        num_of_layers=5
+    )
+    :param max_len:
+    :param vocab_size:
+    :param embed_dim:
+    :param num_heads:
+    :param num_of_layers:
+    :return:
+    """
+    concept_inputs = tf.keras.layers.Input(
+        shape=(max_len,),
+        dtype=tf.int32,
+        name='concept_ids'
+    )
+
+    look_ahead_mask_base = tf.cast(
+        1 - tf.linalg.band_part(tf.ones((max_len, max_len)), -1, 0),
+        dtype=tf.int32
+    )[tf.newaxis, tf.newaxis, :, :]
+
+    embedding_layer = TokenAndPositionEmbedding(max_len, vocab_size, embed_dim)
     x = embedding_layer(concept_inputs)
 
-    transformer_block = GptDecoder(6, embed_dim, num_heads)
-    x, _ = transformer_block(x, decoder_mask_inputs_expanded)
+    transformer_block = GptDecoder(num_of_layers, embed_dim, num_heads)
+    x, _ = transformer_block(x, look_ahead_mask_base)
+
     outputs = tf.keras.layers.Dense(vocab_size)(x)
-    model = tf.keras.Model(inputs=[concept_inputs, decoder_mask_inputs], outputs=[outputs])
-    return model
+    return tf.keras.Model(inputs=[concept_inputs], outputs=[outputs])
