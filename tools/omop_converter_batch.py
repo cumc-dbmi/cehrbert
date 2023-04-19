59dda33648e065b00d4591c6859544d6aaa34993
Zoey Jiang <xj2193@cumc.columbia.edu>
2023-04-18 23:14:14 -0400

    Remove table_name check

--- tools/omop_converter_batch.py
+++ tools/omop_converter_batch.py
@@ -47,8 +47,8 @@ def generate_omop_concept_domain(concept_parquet):
 
 
 def append_to_dict(export_dict, omop_entity, id):
-    if omop_entity.get_table_name() not in export_dict:
-        export_dict[omop_entity.get_table_name()] = {}
+    # if omop_entity.get_table_name() not in list(export_dict.keys):
+    #     export_dict[omop_entity.get_table_name()] = {}
     export_dict[omop_entity.get_table_name()][id] = omop_entity
     return export_dict
 
