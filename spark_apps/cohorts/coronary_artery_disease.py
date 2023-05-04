80f38288ec427f449e0fd1e99cfe24b188f8fe1c
Chao Pang <ChaoPang229@gmail.com>
2023-05-04 13:59:33 -0400

    - updated spark_apps/cohorts/coronary_artery_disease.py to exclude the patients who had a prior graft or stent procedure - added the CABG cohort

--- spark_apps/cohorts/coronary_artery_disease.py
+++ spark_apps/cohorts/coronary_artery_disease.py
@@ -1,6 +1,17 @@
 from spark_apps.cohorts.query_builder import QueryBuilder, QuerySpec, AncestorTableSpec
 
 COHORT_QUERY_TEMPLATE = """
+WITH prior_graft_stent AS (
+    SELECT
+        po.person_id,
+        po.procedure_date
+    FROM global_temp.procedure_occurrence AS po
+    WHERE EXISTS (
+        SELECT 1
+        FROM global_temp.{graft_stent_table} AS gs
+        WHERE po.procedure_concept_id = gs.concept_id
+    )
+)
 SELECT DISTINCT
     c.person_id,
     c.index_date,
@@ -22,13 +33,23 @@ FROM
         WHERE co.condition_concept_id = ie.concept_id
     )
 ) c
-WHERE c.index_date >= '{date_lower_bound}'
+WHERE NOT EXISTS (
+    -- The patients who had a graft or stent procedures before the index date 
+    -- need to be removed from the cohort
+    SELECT 1
+    FROM prior_graft_stent AS exclusion
+    WHERE exclusion.person_id = c.person_id 
+        AND c.index_date > exclusion.procedure_date
+) AND c.index_date >= '{date_lower_bound}' 
 """
 
 DEFAULT_COHORT_NAME = 'coronary_artery_disease'
 DEPENDENCY_LIST = ['person', 'condition_occurrence', 'visit_occurrence']
 CAD_INCLUSION_TABLE = 'CAD'
 CAD_CONCEPTS = [317576]
+
+PRIOR_PROCEDURE_TABLE = 'graft_stent'
+PRIOR_PROCEDURES = [4296227, 42537730, 762043, 44782770, 42537729]
 
 
 def query_builder(spark_args):
@@ -37,6 +58,7 @@ def query_builder(spark_args):
         query_template=COHORT_QUERY_TEMPLATE,
         parameters={
             'cad_concept_table': CAD_INCLUSION_TABLE,
+            'graft_stent_table': PRIOR_PROCEDURE_TABLE,
             'date_lower_bound': spark_args.date_lower_bound
         }
     )
@@ -46,8 +68,12 @@ def query_builder(spark_args):
             table_name=CAD_INCLUSION_TABLE,
             ancestor_concept_ids=CAD_CONCEPTS,
             is_standard=True
+        ),
+        AncestorTableSpec(
+            table_name=PRIOR_PROCEDURE_TABLE,
+            ancestor_concept_ids=PRIOR_PROCEDURES,
+            is_standard=True
         )
-
     ]
     return QueryBuilder(
         cohort_name=DEFAULT_COHORT_NAME,
