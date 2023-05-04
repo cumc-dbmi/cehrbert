51c944e5861afbab543b2240f1c8630a36ff9b48
Chao Pang <ChaoPang229@gmail.com>
2023-05-04 14:01:09 -0400

    added spark_apps/prediction_cohorts/cad_cabg_cohort.py cohort

---
+++ spark_apps/prediction_cohorts/cad_cabg_cohort.py
@@ -0,0 +1,18 @@
+from spark_apps.spark_parse_args import create_spark_args
+from spark_apps.cohorts import coronary_artery_disease as cad
+from spark_apps.cohorts import cabg
+from spark_apps.cohorts.spark_app_base import create_prediction_cohort
+
+DOMAIN_TABLE_LIST = ['condition_occurrence', 'drug_exposure', 'procedure_occurrence']
+
+if __name__ == '__main__':
+    spark_args = create_spark_args()
+
+    ehr_table_list = spark_args.ehr_table_list if spark_args.ehr_table_list else DOMAIN_TABLE_LIST
+
+    create_prediction_cohort(
+        spark_args,
+        cad.query_builder(spark_args),
+        cabg.query_builder(),
+        ehr_table_list
+    )
