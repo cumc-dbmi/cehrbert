e3f554ad9b1296639c3bb58b1284465eed23c1a2
Chao Pang <ChaoPang229@gmail.com>
2023-05-31 09:17:20 -0400

    Added test_encoder_layer_mask to encoder_layer_test.py to test the attention mask

--- tests/unit_tests/models/encoder_layer_test.py
+++ tests/unit_tests/models/encoder_layer_test.py
@@ -5,6 +5,8 @@ from models.custom_layers import EncoderLayer
 
 class EncoderLayerTest(unittest.TestCase):
     def setUp(self):
+        self.batch_size = 32
+        self.input_seq_len = 10
         self.d_model = 512
         self.num_heads = 8
         self.dff = 2048
@@ -12,21 +14,39 @@ class EncoderLayerTest(unittest.TestCase):
         self.encoder_layer = EncoderLayer(self.d_model, self.num_heads, self.dff, self.rate)
 
     def test_call(self):
-        batch_size = 32
-        input_seq_len = 10
-
-        x = tf.random.uniform((batch_size, input_seq_len, self.d_model))
-        mask = tf.random.uniform((batch_size, 1, 1, input_seq_len), minval=0, maxval=2, dtype=tf.int32)
+        x = tf.random.uniform((self.batch_size, self.input_seq_len, self.d_model))
+        mask = tf.random.uniform((self.batch_size, 1, 1, self.input_seq_len), minval=0, maxval=2,
+                                 dtype=tf.int32)
 
         output, attn_weights = self.encoder_layer.call(x, mask)
 
         # Assert output shape
-        self.assertEqual(output.shape, tf.TensorShape([batch_size, input_seq_len, self.d_model]))
+        self.assertEqual(output.shape,
+                         tf.TensorShape([self.batch_size, self.input_seq_len, self.d_model]))
 
         # Assert attn_weights shape
-        self.assertEqual(attn_weights.shape, tf.TensorShape([batch_size, self.num_heads, input_seq_len, input_seq_len]))
+        self.assertEqual(
+            attn_weights.shape,
+            tf.TensorShape(
+                [self.batch_size, self.num_heads, self.input_seq_len, self.input_seq_len])
+        )
 
-        # Add additional assertions based on the behavior of your EncoderLayer
+    def test_encoder_layer_mask(self):
+        x = tf.random.normal((self.batch_size, self.input_seq_len, self.d_model))
+        # Block the attention to the last two positions of the sequence in the batch
+        encoder_mask = tf.tile(
+            tf.expand_dims(
+                tf.constant([0] * (self.input_seq_len - 2) + [1, 1]), axis=0
+            ),
+            [self.batch_size, 1]
+        )[:, tf.newaxis, tf.newaxis, :]
+
+        output, attn_weights_block = self.encoder_layer(
+            x,
+            encoder_mask
+        )
+        # Attention weights should be 0 since they are masked
+        self.assertEqual(0, tf.reduce_sum(attn_weights_block[:, :, :, -2:]))
 
 
 if __name__ == '__main__':
