6cf0f4d438aacf6349c7f7f83760f04388658ef7
Chao Pang <ChaoPang229@gmail.com>
2023-06-17 22:06:31 -0400

    added day_tokens to generate_artificial_time_tokens in gpt_model

--- models/gpt_model.py
+++ models/gpt_model.py
@@ -380,10 +380,11 @@ def generate_artificial_time_tokens():
     Generate all the time tokens used in training
     :return:
     """
+    day_tokens = [f'D{i}' for i in range(2000)]
     week_tokens = [f'W{i}' for i in range(4)]
     month_tokens = [f'M{i}' for i in range(12)]
     long_term_tokens = ['LT']
-    return week_tokens + month_tokens + long_term_tokens
+    return day_tokens + week_tokens + month_tokens + long_term_tokens
 
 
 def generate_visit_boundary_tokens():
