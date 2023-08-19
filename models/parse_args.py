a915ca96df0b1fdf6542fe7e967496dbc4a8741f
Chao Pang <ChaoPang229@gmail.com>
2023-08-19 09:50:01 -0400

    added the including_long_sequence option to the GPT trainer to include the long patient sequences by slicing out a portion of their data

--- models/parse_args.py
+++ models/parse_args.py
@@ -233,6 +233,11 @@ def create_parse_args_gpt():
         default=256,
         required=False
     )
+    parser.add_argument(
+        '--including_long_sequence',
+        dest='including_long_sequence',
+        action='store_true'
+    )
     return parser
 
 
