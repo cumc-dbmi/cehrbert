c8490f3c72bb377b560bf191b77d383ed693c546
Xinzhuo Jiang <xj2193@cumc.columbia.edu>
2024-03-21 16:17:13 -0400

    update the code

--- analyses/gpt/privacy/attribute_inference/attribute_inference_matrix_operation.py
+++ analyses/gpt/privacy/attribute_inference/attribute_inference_matrix_operation.py
@@ -38,26 +38,22 @@ def main(
     LOG.info(f'Started loading training data at {args.training_data_folder}')
     train_data = pd.read_parquet(args.training_data_folder)
 
-    LOG.info(f'Started loading evaluation data at {args.evaluation_data_folder}')
-    evaluation_data = pd.read_parquet(args.evaluation_data_folder)
-
     LOG.info(f'Started loading synthetic_data at {args.synthetic_data_folder}')
     synthetic_data = pd.read_parquet(args.synthetic_data_folder)
 
     LOG.info('Started extracting the demographic information from the patient sequences')
     train_data = create_demographics(train_data)
-    evaluation_data = create_demographics(evaluation_data)
     synthetic_data = create_demographics(synthetic_data)
 
     LOG.info('Started rescaling age columns')
     train_data = scale_age(train_data)
-    evaluation_data = scale_age(evaluation_data)
     synthetic_data = scale_age(synthetic_data)
 
     LOG.info('Started encoding gender')
     gender_encoder = create_gender_encoder(
         train_data,
-        evaluation_data,
+        # TODO need to change this function to be generic
+        train_data[:10],
         synthetic_data
     )
     LOG.info('Completed encoding gender')
@@ -65,78 +61,78 @@ def main(
     LOG.info('Started encoding race')
     race_encoder = create_race_encoder(
         train_data,
-        evaluation_data,
+        # TODO need to change this function to be generic
+        train_data[:10],
         synthetic_data
     )
     LOG.info('Completed encoding race')
 
-    random.seed(RANDOM_SEE)
-    all_results = []
-    for i in range(1, args.n_iterations + 1):
-        dist_metrics = []
-        LOG.info(f'Iteration {i} Started')
-        LOG.info(f'Iteration {i}: Started creating train vectors')
-        train_common_vectors, train_sensitive_vectors = create_vector_representations_for_attribute(
-            train_data,
-            concept_tokenizer,
-            gender_encoder,
-            race_encoder,
-            common_attributes=common_attributes,
-            sensitive_attributes=sensitive_attributes
-        )
+    LOG.info(f'Started creating train vectors')
+    train_common_vectors, train_sensitive_vectors = create_vector_representations_for_attribute(
+        train_data,
+        concept_tokenizer,
+        gender_encoder,
+        race_encoder,
+        common_attributes=common_attributes,
+        sensitive_attributes=sensitive_attributes
+    )
 
-        LOG.info(f'Iteration {i}: Started creating synthetic vectors')
-        synthetic_common_vectors, synthetic_sensitive_vectors = create_vector_representations_for_attribute(
-            synthetic_data,
-            concept_tokenizer,
-            gender_encoder,
-            race_encoder,
-            common_attributes=common_attributes,
-            sensitive_attributes=sensitive_attributes
-        )
+    LOG.info(f'Started creating synthetic vectors')
+    synthetic_common_vectors, synthetic_sensitive_vectors = create_vector_representations_for_attribute(
+        synthetic_data,
+        concept_tokenizer,
+        gender_encoder,
+        race_encoder,
+        common_attributes=common_attributes,
+        sensitive_attributes=sensitive_attributes
+    )
 
-        LOG.info(f'Iteration {i}: Started calculating the distances between synthetic and training vectors')
-        synthetic_train_index = batched_pairwise_euclidean_distance_indices(train_common_vectors,
-                                                                            synthetic_common_vectors,
-                                                                            batch_size=args.matching_batch_size)
-        train_train_index = batched_pairwise_euclidean_distance_indices(train_common_vectors, train_common_vectors,
-                                                                        batch_size=args.matching_batch_size,
-                                                                        self_exclude=True)
+    LOG.info(f'Started calculating the distances between synthetic and training vectors')
+    train_synthetic_index = batched_pairwise_euclidean_distance_indices(train_common_vectors,
+                                                                        synthetic_common_vectors,
+                                                                        batch_size=args.matching_batch_size)
+    train_train_index = batched_pairwise_euclidean_distance_indices(train_common_vectors, train_common_vectors,
+                                                                    batch_size=args.matching_batch_size,
+                                                                    self_exclude=True)
 
-        def cal_f1_score(vector_a, vector_b, index_matrix):
-            shared_vector = np.logical_and(vector_a[index_matrix], vector_b[:index_matrix]).astype(int)
-            shared_vector_sum = np.sum(shared_vector, axis=1)
-            precision = shared_vector_sum / np.sum(vector_a, axis=1)
-            recall = shared_vector_sum / np.sum(vector_b, axis=1)
-            if recall > 0 and precision > 0:
-                f1 = 2 * recall * precision / (recall + precision)
-            else:
-                f1 = None
-            return f1, precision, recall
+    def save_divide(numerator_vector, denominator_vector):
+        return np.where(denominator_vector > 0, numerator_vector/denominator_vector, 0)
 
-        f1_syn_train, precision_syn_train, recall_syn_train = cal_f1_score(train_sensitive_vectors, synthetic_sensitive_vectors, synthetic_train_index)
-        f1_train_train, precision_train_train, recall_train_train = cal_f1_score(train_sensitive_vectors, train_sensitive_vectors, train_train_index)
+    def cal_f1_score(vector_a, vector_b, index_matrix):
+        # vector_a is train data and vector_b is synthetic data or iteself
+        shared_vector = np.logical_and(vector_a[:len(index_matrix)], vector_b[index_matrix]).astype(int)
+        shared_vector_sum = np.sum(shared_vector, axis=1)
 
-        results = {
-            "Iteration": i,
-            "Precision Synthetic Train": precision_syn_train,
-            "Recall Synthetic Train": recall_syn_train,
-            "F1 Synthetic Train": f1_syn_train,
-            "Precision Train Train": precision_train_train,
-            "Recall Train Train": recall_train_train,
-            "F1 Train Train": f1_train_train
-        }
-        all_results.append(results)
-        LOG.info(f'Iteration {i}: Attribute Inference Risk {results}')
+        precision = save_divide(shared_vector_sum, np.sum(vector_b, axis=1))
+        recall = save_divide(shared_vector_sum, np.sum(vector_a, axis=1))
+
+        f1 = save_divide(2 * recall * precision, recall + precision)
+        return f1, precision, recall
+
+    f1_syn_train, precision_syn_train, recall_syn_train = cal_f1_score(train_sensitive_vectors, synthetic_sensitive_vectors, train_synthetic_index)
+    f1_train_train, precision_train_train, recall_train_train = cal_f1_score(train_sensitive_vectors, train_sensitive_vectors, train_train_index)
+
+    results = {
+        "Precision Synthetic Train": precision_syn_train,
+        "Recall Synthetic Train": recall_syn_train,
+        "F1 Synthetic Train": f1_syn_train,
+        "Precision Train Train": precision_train_train,
+        "Recall Train Train": recall_train_train,
+        "F1 Train Train": f1_train_train
+    }
+    LOG.info(f'Attribute Inference: Average Precision Synthetic Train: {np.mean(precision_syn_train)} \n'
+             f'Attribute Inference: Average Recall Synthetic Train:{np.mean(recall_syn_train)} \n'
+             f'Attribute Inference: Average F1 Synthetic Train: {np.mean(f1_syn_train)} \n'
+             f'Attribute Inference: Average Precision Train Train: {np.mean(precision_train_train)} \n'
+             f'Attribute Inference: Average Recall Train Train: {np.mean(recall_train_train)} \n'
+             f'Attribute Inference: Average F1 Train Train: {np.mean(f1_train_train)}')
 
     current_time = datetime.now().strftime("%m-%d-%Y-%H-%M-%S")
     pd.DataFrame(
-        all_results,
-        columns=["Iteration", "Precision Synthetic Train", "Recall Synthetic Train", "F1 Synthetic Train",
+        results,
+        columns=["Precision Synthetic Train", "Recall Synthetic Train", "F1 Synthetic Train",
                  "Precision Train Train", "Recall Train Train",  "F1 Train Train"]
-    ).to_parquet(
-        os.path.join(args.output_folder, f"attribute_inference_{current_time}.parquet")
-    )
+    ).to_parquet(os.path.join(args.output_folder, f"attribute_inference_{current_time}.parquet"))
 
 
 def create_argparser():
@@ -149,13 +145,6 @@ def create_argparser():
         dest='training_data_folder',
         action='store',
         help='The path for where the training data folder',
-        required=True
-    )
-    parser.add_argument(
-        '--evaluation_data_folder',
-        dest='evaluation_data_folder',
-        action='store',
-        help='The path for where the evaluation data folder',
         required=True
     )
     parser.add_argument(
@@ -178,14 +167,6 @@ def create_argparser():
         action='store',
         help='The path to ConceptTokenizer',
         required=True
-    )
-    parser.add_argument(
-        '--n_iterations',
-        dest='n_iterations',
-        action='store',
-        type=int,
-        required=False,
-        default=1
     )
     parser.add_argument(
         '--attribute_config',
