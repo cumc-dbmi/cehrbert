febfb0e4e8101c3984140fc61f5d43a5ec10f412
Chao Pang <ChaoPang229@gmail.com>
2024-04-22 16:16:54 -0400

    set the visit_concept_order of the ATT token the same as the next visit

--- spark_apps/decorators/patient_event_decorator.py
+++ spark_apps/decorators/patient_event_decorator.py
@@ -280,7 +280,7 @@ class PatientEventAttDecorator(PatientEventDecorator):
             .withColumn('standard_concept_id', time_token_udf('time_delta')) \
             .withColumn('priority', F.lit(-3)) \
             .withColumn('visit_rank_order', F.col('visit_rank_order')) \
-            .withColumn('visit_concept_order', F.lit(0)) \
+            .withColumn('visit_concept_order', F.col('min_visit_concept_order')) \
             .withColumn('concept_order', F.lit(0)) \
             .drop('prev_visit_end_date', 'time_delta') \
             .drop('min_visit_concept_order', 'max_visit_concept_order') \
