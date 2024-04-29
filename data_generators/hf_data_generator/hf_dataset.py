f5abb5ec135cfff0cb66783d372b35cee617cc95
Chao Pang <ChaoPang229@gmail.com>
2024-04-28 21:48:20 -0400

    added index_date to the finetuning dataset

--- data_generators/hf_data_generator/hf_dataset.py
+++ data_generators/hf_data_generator/hf_dataset.py
@@ -17,7 +17,7 @@ CEHRBERT_COLUMNS = [
 
 TRANSFORMER_COLUMNS = ['input_ids', 'labels']
 
-FINETUNING_COLUMNS = ['age_at_index', 'classifier_label']
+FINETUNING_COLUMNS = ['age_at_index', 'classifier_label', 'index_date']
 
 
 def create_cehrbert_pretraining_dataset(
