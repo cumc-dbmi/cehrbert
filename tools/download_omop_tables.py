6e03a60fff9358f86c954bdb833e3479bfd2b51b
Chao Pang <ChaoPang229@gmail.com>
2024-08-23 22:31:18 -0400

    Added patient_splits to download_omop_tables.py

--- tools/download_omop_tables.py
+++ tools/download_omop_tables.py
@@ -1,12 +1,16 @@
 import configparser
 import argparse
+import os.path
+
 from pyspark.sql import SparkSession
+from pyspark.sql import functions as f
 
 omop_table_dict = {'person': 'person_id', 'condition_occurrence': 'condition_occurrence_id',
                    'measurement': 'measurement_id',
                    'drug_exposure': 'drug_exposure_id',
                    'procedure_occurrence': 'procedure_occurrence_id',
                    'observation': 'observation_id', 'visit_occurrence': 'visit_occurrence_id'}
+
 
 def find_num_of_records(
         domain_table_name,
@@ -109,4 +113,14 @@ if __name__ == "__main__":
             print('table: ' + str(item) + ' is downloaded')
         except Exception as e:
             print(str(e))
+
     print('The following tables were downloaded:' + str(downloaded_tables))
+    patient_splits_folder = os.path.join(download_folder, "patient_splits")
+    if not os.path.exists(patient_splits_folder):
+        print('Creating the patient splits')
+        person = spark.read.parquet(os.path.join(download_folder, "person"))
+        train_split, test_split = person.select('person_id').randomSplit([0.8, 0.2], seed=42)
+        train_split = train_split.withColumn("split", f.lit("train"))
+        test_split = test_split.withColumn("split", f.lit("test"))
+        patient_splits = train_split.unionByName(test_split)
+        patient_splits.write.parquet(os.path.join(download_folder, "patient_splits"))
