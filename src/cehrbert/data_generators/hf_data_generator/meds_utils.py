8d453cbb68132cf299f17c604a1dc292be6f9a19
Chao Pang <ChaoPang229@gmail.com>
2024-10-25 13:17:31 -0400

    check whether patient_id exists in the meds_reader database because subject_splits can contain more patients than the dataset

--- src/cehrbert/data_generators/hf_data_generator/meds_utils.py
+++ src/cehrbert/data_generators/hf_data_generator/meds_utils.py
@@ -221,8 +221,9 @@ def _meds_to_cehrbert_generator(
     with meds_reader.SubjectDatabase(path_to_db) as patient_database:
         for shard in shards:
             for patient_id, prediction_time, label in shard:
-                patient = patient_database[patient_id]
-                yield convert_one_patient(patient, conversion, prediction_time, label)
+                if patient_id in patient_database:
+                    patient = patient_database[patient_id]
+                    yield convert_one_patient(patient, conversion, prediction_time, label)
 
 
 def _create_cehrbert_data_from_meds(
