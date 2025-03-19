c0260054830b9556ca129c2a2485d2142fdd2d20
Chao Pang <ChaoPang229@gmail.com>
2025-03-19 00:29:55 -0400

    fixed the merging visit logging information

--- src/cehrbert/data_generators/hf_data_generator/patient_block.py
+++ src/cehrbert/data_generators/hf_data_generator/patient_block.py
@@ -393,8 +393,8 @@ def merge_patient_blocks(patient: meds_reader.Subject, patient_blocks: List[Pati
         debug_log = ""
         for prev_v, prev_min_t, prev_max_t, next_v, next_min_t, next_max_t in merging_info:
             debug_log += (
-                f"{patient.subject_id}: visit {prev_v} with {prev_min_t} and {prev_max_t} "
-                f"has been merged into visit {next_v} with {next_min_t} and {next_max_t}"
+                f"{patient.subject_id}: visit {next_v} with {next_min_t} and {next_max_t} "
+                f"has been merged into visit {prev_v} with {prev_min_t} and {prev_max_t}"
             )
         LOG.debug(debug_log)
 
